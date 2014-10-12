7963bb37af9e5d3eb9d8aade9ba26af3384e32a6
andilab <andrzej.kostanski@gmail.com>
2014-10-12 15:37:40 +0200

    Update views.py

--- demo/views.py
+++ demo/views.py
@@ -451,7 +451,7 @@ def qrencode_vcard(request, pk, size=3):
 
 def qrencode_link(request, pk, size=3):
 
-    dane = reverse('spot-detail', kwargs={'pk': pk})
+    dane = "%s/%s" %(request.build_absolute_uri(), reverse('spot-detail', kwargs={'pk': pk}))
     img = make_qrcode(dane, box_size=size)
 
     response = HttpResponse(content_type="image/png")
