57d472be8d556ad527d8169dec10b715261e69ec
Andrzej Kostanski <andrzej.kostanski@daftcode.pl>
2014-11-16 02:04:43 +0100

    fix in set

--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mbf.settings.DJANGO_SETTINGS_MODULE")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mbf.settings.production")
 
     from django.core.management import execute_from_command_line
 
