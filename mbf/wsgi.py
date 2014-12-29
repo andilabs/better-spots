b435c0a7b4976d1d9644fe1f099169a4a04b6878
andrzej.kostanski <andrzej.kostanski@daftcode.pl>
2014-12-29 03:16:20 +0100

    setty

--- mbf/wsgi.py
+++ mbf/wsgi.py
@@ -8,22 +8,8 @@ https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 
 import os
-import sys
-#import site
 
-#envpath = '/home/ubuntu/.virtualenvs/dogspot/lib/python2.7/site-packages'
-
-# we add currently directory to path and change to it
-#pwd = os.path.dirname(os.path.abspath(__file__))
-#os.chdir(pwd)
-#sys.path = [pwd] + sys.path
-
-# Append paths
-#site.addsitedir(envpath)
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mbf.settings.settings")
-#sys.path.append('/home/ubuntu/dogspot.eu')
-# os.environ['DJANGO_SETTINGS_MODULE'] = 'mbf.settings'
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mbf.settings")
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
