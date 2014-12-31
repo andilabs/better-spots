dcf2c861faa0a8b60856a733902080625359d6c5
andrzej.kostanski <andrzej.kostanski@daftcode.pl>
2014-12-31 16:31:58 +0100

    setty

--- mbf/settings/enabledspot.py
+++ mbf/settings/enabledspot.py
@@ -14,6 +14,7 @@ DATABASES = {
         'ENGINE': 'django.contrib.gis.db.backends.postgis',
         'NAME': 'enabledspot',
         'USER': 'enabledspot',
+        'PASSWORD': 'c9c38a6dc8cdb66a0c416a9e1f8eac21',
         'HOST': 'localhost',
         'PORT': '5432',
     }
