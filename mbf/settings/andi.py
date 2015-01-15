f67d7a72975685de8c2ae17f02b98140806befad
andrzej.kostanski <andrzej.kostanski@daftcode.pl>
2015-01-15 01:19:47 +0100

    logos in mission

--- mbf/settings/andi.py
+++ mbf/settings/andi.py
@@ -41,16 +41,16 @@ STATIC_ROOT = '/Users/andi/dogspot_pseudo_static_media_server/'
 # SPOT_PROJECT_DESCRIPTION = 'Is an app for reviewing spots in terms of being enabled for people with disabilites'
 # SPOT_PROJECT_SUBJECT = 'Disabled'
 
-SPOT_PROJECT_NAME = 'momspot'
-SPOT_PROJECT_SLOGAN = 'Moms\' Revolution starts in cafes!'
-SPOT_PROJECT_DESCRIPTION = 'Is an app for reviewing moms and childrens friendly spots'
-SPOT_PROJECT_SUBJECT = 'Moms'
+# SPOT_PROJECT_NAME = 'momspot'
+# SPOT_PROJECT_SLOGAN = 'Moms\' Revolution starts in cafes!'
+# SPOT_PROJECT_DESCRIPTION = 'Is an app for reviewing moms and childrens friendly spots'
+# SPOT_PROJECT_SUBJECT = 'Moms'
 
-# SPOT_PROJECT_NAME = 'dogspot'
-# SPOT_PROJECT_SLOGAN = 'Wow the World !'
-# SPOT_PROJECT_DESCRIPTION = 'Is an app for exploring and reviewing dog friendly spots'
-# SPOT_PROJECT_SUBJECT = 'Dog'
+SPOT_PROJECT_NAME = 'dogspot'
+SPOT_PROJECT_SLOGAN = 'Wow the World !'
+SPOT_PROJECT_DESCRIPTION = 'Is an app for exploring and reviewing dog friendly spots'
+SPOT_PROJECT_SUBJECT = 'Dog'
 
 SPOT_PROJECT_FAVICON_URL = os.path.join('media', SPOT_PROJECT_NAME, SPOT_PROJECT_NAME + '_favicon.ico')
 SPOT_PROJECT_BRAND_LOGO_URL = os.path.join('media', SPOT_PROJECT_NAME, SPOT_PROJECT_NAME + '_logo.png')
-SPOT_PROJECT_CERTIFICATE_LOGO_URL = os.path.join(MEDIA_ROOT, SPOT_PROJECT_NAME, SPOT_PROJECT_NAME + '_certificate.png')
+SPOT_PROJECT_CERTIFICATE_LOGO_URL = os.path.join('media', SPOT_PROJECT_NAME, SPOT_PROJECT_NAME + '_certificate.png')
