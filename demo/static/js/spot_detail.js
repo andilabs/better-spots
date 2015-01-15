486ebdb5bdb2fd15c6c64e6a1263b013116e49a5
andrzej.kostanski <andrzej.kostanski@daftcode.pl>
2015-01-15 01:47:11 +0100

    ch

--- demo/static/js/spot_detail.js
+++ demo/static/js/spot_detail.js
@@ -13,10 +13,10 @@
         bounds: true,
         dogs_allowed: ['lapka'],
         spot_type: ['lapka'],
-        icon: {
-          url: STATIC_URL + 'lapka_icon.png',
-          size: new google.maps.Size(50, 50)
-        }
+        // icon: {
+        //   url: STATIC_URL,
+        //   size: new google.maps.Size(50, 50)
+        // }
       });
       $("#map_canvas_spot_detail").gmap("option", "zoom", 17);
       return $("#dgos_allowance").on("click", function(e) {
