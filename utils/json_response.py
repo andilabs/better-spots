d9ddde1c813106dd5211622b0140f84765d7c538
andrzej.kostanski <andrzej.kostanski@daftcode.pl>
2015-01-29 17:20:10 +0100

    fix

--- utils/json_response.py
+++ utils/json_response.py
@@ -9,7 +9,7 @@ class JSONResponse(HttpResponse):
     """
     An HttpResponse that renders its content into JSON.
 
-    Django VERSION 1.7 will offer JSONResponse
+    After switch to Django VERSION 1.7 which offer JsonResponse NO MORE NEEDED
     https://docs.djangoproject.com/en/1.7/ref/request-response/#jsonresponse-objects
     """
     def __init__(self, data, **kwargs):
