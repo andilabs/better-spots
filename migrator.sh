109507280da181f2391438f0825c901fe9ed25c8
andi <andi@andilabs.com>
2015-02-04 00:41:17 +0100

    ch for pro

--- migrator.sh
+++ migrator.sh
@@ -3,23 +3,23 @@ rm -rf accounts/migrations
 rm -rf core/migrations
 rm -rf www/migrations
 
-python manage.py schemamigration accounts --initial --settings=mbf.settings.andi
-python manage.py schemamigration core --initial --settings=mbf.settings.andi
-python manage.py schemamigration www --initial --settings=mbf.settings.andi
+python manage.py schemamigration accounts --initial
+python manage.py schemamigration core --initial
+python manage.py schemamigration www --initial
 
-python manage.py syncdb --settings=mbf.settings.andi
+python manage.py syncdb
 
 
-python manage.py migrate accounts --settings=mbf.settings.andi
-python manage.py migrate core --settings=mbf.settings.andi
-python manage.py migrate www --settings=mbf.settings.andi
+python manage.py migrate accounts
+python manage.py migrate core
+python manage.py migrate www
 
-python manage.py migrate easy_thumbnails --settings=mbf.settings.andi
-python manage.py migrate rest_framework.authtoken --settings=mbf.settings.andi
-python manage.py migrate django_extensions --settings=mbf.settings.andi
+python manage.py migrate easy_thumbnails
+python manage.py migrate rest_framework.authtoken
+python manage.py migrate django_extensions
 
-python manage.py create_andi --settings=mbf.settings.andi
+python manage.py create_andi
 cp -r data/"$instance_name"/initial_img/. media/img
-python manage.py loaddata data/"$instance_name"/spots.json --settings=mbf.settings.andi
-python manage.py create_slugs --settings=mbf.settings.andi
-python manage.py runserver --settings=mbf.settings.andi
+python manage.py loaddata data/"$instance_name"/spots.json
+python manage.py create_slugs
+python manage.py runserver
