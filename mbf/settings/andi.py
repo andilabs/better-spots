7179a59f967373566b956bf3323d7bad5db8a842
andi <andi@andilabs.com>
2015-06-04 16:16:52 +0200

    design

--- mbf/settings/andi.py
+++ mbf/settings/andi.py
@@ -177,3 +177,31 @@ SPOT_PROJECT_FAVICON_URL = os.path.join(
 )
 
 SPOT_PROJECT_BLOGERS = os.path.join(STATIC_URL, SPOT_PROJECT_NAME, 'bloger_photo.jpg')
+
+MIDDLEWARE_CLASSES = ('utils.performance.db_queries.QueryCountDebugMiddleware',) + MIDDLEWARE_CLASSES
+
+LOGGING = {
+    'disable_existing_loggers': False,
+    'version': 1,
+    'handlers': {
+        'console': {
+            # logging handler that outputs log messages to terminal
+            'class': 'logging.StreamHandler',
+            'level': 'DEBUG', # message level to be written to console
+        },
+    },
+    'loggers': {
+        '': {
+            # this sets root level logger to log debug and higher level
+            # logs to console. All other loggers inherit settings from
+            # root level logger.
+            'handlers': ['console'],
+            'level': 'DEBUG',
+            'propagate': False, # this tells logger to send logging message
+                                # to its parent (will send if set to True)
+        },
+        'django.db': {
+            # django also has database level logging
+        },
+    },
+}
