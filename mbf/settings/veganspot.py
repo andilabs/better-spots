9a65ba1345b447e2d0303487f58d60b397bf2772
andi <andi@andilabs.com>
2016-01-09 14:27:07 +0100

    new settings for new veganspot doamin

--- mbf/settings/veganspot.py
+++ mbf/settings/veganspot.py
@@ -1,20 +1,20 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-INSTANCE_DOMAIN = "veganspot.org"
+INSTANCE_DOMAIN = "veganspot.eu"
 
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'poczta.superhost.pl'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'no-reply@veganspot.org'
+EMAIL_HOST_USER = 'no-reply@veganspot.eu'
 EMAIL_HOST_PASSWORD = 'c9c38a6dc8cdb66a0c416a9e1f8eac21'
 
 RAVEN_CONFIG = {
     'dsn': 'https://c487d89198d844f1b3b2271833301fdd:2b007aed601c4cacaa72ce45443f3db7@app.getsentry.com/37314',
 }
 
-STATIC_ROOT = '/home/ubuntu/veganspot.org/static_assets/'
-MEDIA_ROOT = '/home/ubuntu/veganspot.org/media_assets/'
+STATIC_ROOT = '/home/ubuntu/veganspot.eu/static_assets/'
+MEDIA_ROOT = '/home/ubuntu/veganspot.eu/media_assets/'
 
 POSTGIS_VERSION = (2, 1, 2)
 
