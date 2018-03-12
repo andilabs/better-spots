ac2d2f051122edd3af0253e13931f0bc380a67cf
andi <andi@andilabs.com>
2018-03-12 01:09:13 +0100

    added tests for google api reverse geocoding

--- utils/geocoding.py
+++ utils/geocoding.py
@@ -3,6 +3,8 @@ import requests
 from django.conf import settings
 from django.utils.http import urlquote
 
+GEOCODING_URL = "http://maps.googleapis.com/maps/api/geocode/json?address={address}&sensor=false"
+
 
 def geocoding(address):
     """ makes request to Google Maps API, and returns
@@ -11,7 +13,7 @@ def geocoding(address):
     :param address: string in form like: 'Warszawa Kolejowa 47'
     :return: dict with latitude and longitude like: {u'lat': 52.2263065, u'lng': 20.9828942}
     """
-    url = "http://maps.googleapis.com/maps/api/geocode/json?address={address}&sensor=false".format(
+    url = GEOCODING_URL.format(
         address=urlquote(address.replace(' ', '+'))
     )
     response = requests.get(url)
@@ -19,9 +21,12 @@ def geocoding(address):
     return info
 
 
+REVERSE_GEOCODING_URL = "https://maps.googleapis.com/maps/api/geocode/json?latlng={latitude},{longitude}&key={api_key}"
+
+
 def reverse_geocoding(latitude, longitude):
 
-    url = "https://maps.googleapis.com/maps/api/geocode/json?latlng={latitude},{longitude}&key={api_key}".format(
+    url = REVERSE_GEOCODING_URL.format(
         latitude=latitude,
         longitude=longitude,
         api_key=settings.GOOGLE_API_KEY
---
+++ utils/tests.py
@@ -0,0 +1,55 @@
+import json
+import responses
+
+from django.conf import settings
+from django.test import TestCase
+
+from utils.geocoding import reverse_geocoding, REVERSE_GEOCODING_URL
+
+
+RESPONSE_GEOCODING_MOCK = {
+    'results': [
+        {
+            'address_components': [
+                {'long_name': '74', 'short_name': '74', 'types': ['street_number']},
+                {'long_name': 'Mickiewicza', 'short_name': 'Mickiewicza', 'types': ['route']},
+                {'long_name': 'Żoliborz', 'short_name': 'Żoliborz', 'types': ['political', 'sublocality', 'sublocality_level_1']},
+                {'long_name': 'Warszawa', 'short_name': 'Warszawa', 'types': ['locality', 'political']},
+                {'long_name': 'Warszawa', 'short_name': 'Warszawa', 'types': ['administrative_area_level_2', 'political']},
+                {'long_name': 'mazowieckie', 'short_name': 'mazowieckie', 'types': ['administrative_area_level_1', 'political']},
+                {'long_name': 'Poland', 'short_name': 'PL', 'types': ['country', 'political']}
+            ],
+            'formatted_address': 'Mickiewicza 74, Warszawa, Poland',
+            'geometry': {'location': {'lat': 52.2790223, 'lng': 20.980648},
+                         'location_type': 'ROOFTOP',
+                         'viewport': {'northeast': {'lat': 52.2803712802915, 'lng': 20.9819969802915},
+                                      'southwest': {'lat': 52.2776733197085, 'lng': 20.9792990197085}}
+                         },
+            'place_id': 'ChIJxXoMrOfLHkcRBAFsrjKGcMc',
+            'types': ['street_address']
+        }
+    ],
+    'status': 'OK'
+}
+
+
+class TestGoogleAPIGeocodingConsumer(TestCase):
+
+    @responses.activate
+    def test_geocoding(self):
+        latitude, longitude = 52.27904, 20.980366
+        responses.add(
+            responses.GET,
+            REVERSE_GEOCODING_URL.format(
+                latitude=latitude,
+                longitude=longitude,
+                api_key=settings.GOOGLE_API_KEY
+            ),
+            body=json.dumps(RESPONSE_GEOCODING_MOCK),
+            content_type="application/json")
+
+        response = reverse_geocoding(latitude, longitude)
+        self.assertEqual(response, {'address_city': 'Warszawa',
+                                    'address_country': 'Poland',
+                                    'address_number': '74',
+                                    'address_street': 'Mickiewicza'})
