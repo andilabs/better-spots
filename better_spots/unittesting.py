f4e12931c71d4a64d97ad811e2ec1c945f9448b5
andi <andi@andilabs.com>
2018-04-11 23:20:12 +0200

    fix db specific for travis

--- better_spots/unittesting.py
+++ better_spots/unittesting.py
@@ -1,18 +1,17 @@
 import os
 from better_spots.settings import *
 
-if 'TRAVIS' in os.environ:
 
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.contrib.gis.db.backends.postgis',
-            'NAME':     'travisci',
-            'USER':     'postgres',
-            'PASSWORD': '',
-            'HOST':     'localhost',
-            'PORT':     '',
-        }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'NAME':     'travisci',
+        'USER':     'postgres',
+        'PASSWORD': '',
+        'HOST':     'localhost',
+        'PORT':     '',
     }
+}
 
 
 class DisableMigrations(object):
