910403ae2245f2ee10e59086e74ca857b7fe4281
andi <andi@andilabs.com>
2018-04-12 14:02:08 +0200

    turn off disable migrations

--- better_spots/unittesting.py
+++ better_spots/unittesting.py
@@ -14,13 +14,13 @@ DATABASES = {
 }
 
 
-class DisableMigrations(object):
-    def __contains__(self, item):
-        return True
-
-    def __getitem__(self, item):
-        return None
-
-
-MIGRATION_MODULES = DisableMigrations()
+# class DisableMigrations(object):
+#     def __contains__(self, item):
+#         return True
+#
+#     def __getitem__(self, item):
+#         return None
+#
+#
+# MIGRATION_MODULES = DisableMigrations()
 
