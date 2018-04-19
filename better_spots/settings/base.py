05061c4ff563380757d4535e1e4807da25b154b6
andi <andi@andilabs.com>
2018-04-19 17:22:21 +0200

    secret key stay secret

--- better_spots/settings/base.py
+++ better_spots/settings/base.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__fil
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'pe47b+y(j7^8u8krhk)2yd5f31z7+16cm4f%)6z7h@hhvlm-5g'
+SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
